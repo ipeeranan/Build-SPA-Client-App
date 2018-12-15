302860e076116090d85227ca1cedfcf7b3457a31
unknown <stroms31@gmail.com>
2018-12-15 15:28:20 +0700

    creatae page

--- vuesss/src/router.js
+++ vuesss/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Orders from './views/Order.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Orders
     },
     {
       path: '/about',
--- vuesss/src/views/About.vue
+++ vuesss/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an Exam Final</h1>
+    <h3>Peeranan Reanmataen</h3>
+    <h5>SE 5930213043</h5>
   </div>
 </template>
