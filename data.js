1a4e6e4953f289491705589a74fc3c021dc0f076
unknown <stroms31@gmail.com>
2018-12-15 14:58:40 +0700

    push

--- data.js
+++ data.js
@@ -2,8 +2,7 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
 
 module.exports = {
-    getOrdersAll,
-    getOrderByID
+    getOrdersAll
 }
 
 function getOrdersAll(req, res) {
@@ -14,20 +13,6 @@ function getOrdersAll(req, res) {
                     status: 'success',
                     data: data,
                     message: 'Retrieved ALL orders'
-                });
-        })
-        .catch(function (error) {
-            console.log('ERROR:', error)
-        })
-}
-function getOrderByID(req, res) {
-    db.any('select * from orders where id =' + req.params.id)
-        .then(function (data) {
-            res.status(200)
-                .json({
-                    status: 'success',
-                    data: data,
-                    message: 'Retrieved products id:' + req.params.id
                 });
         })
         .catch(function (error) {
